f696192e7d29f08c9d0917095e997f41a1d26418
jgraciolli <148507173+jgraciolli@users.noreply.github.com>
2025-06-19 23:43:19 -0300

    build: initial project setup with React

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Welcome to the PokeApp!</h1>
+      </header>
+      <div>
+        <button>
+          <a href=''>Get started!</a>
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
