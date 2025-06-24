94f02c4639b46ed77235ca54cd5eb570fa8f05fa
jgraciolli <148507173+jgraciolli@users.noreply.github.com>
2025-06-24 11:18:46 -0300

    fix: made whole pokeball clickable
    also adjusted alignments in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
 import '../styles/css/home.css';
 import Logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate()
+
     return (
         <>
             <main className='home-container'>
                 <img src={Logo} alt="PokéApp logo" className="logo" />              
                 <h1>Explore todos os Pokémon!</h1>
-                <button className="pokeball-button">
-                    <a href="/pokemons" className="pokeball-link"></a>
+                <button className="pokeball-button"
+                    onClick={() => navigate('/pokemons')}
+                >
+                    <span className="pokeball-link" />
                 </button>
             </main>            
         </>
